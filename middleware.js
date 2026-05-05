84c81844285fbffdc5e99e7e31b3efd116dd84a8
Prashant07-git <prashantvasukar@gmail.com>
2026-05-05 20:44:36 +0530

    remove maintenance mode

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 
 export function middleware(request) {
-  const maintenance = true // 🔴 turn ON/OFF here
+  const maintenance = false // 🔴 turn ON/OFF here
 
   if (maintenance) {
     return NextResponse.rewrite(new URL('/static/maintenance.html', request.url))
